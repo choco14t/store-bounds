a9e4d7aeca04ed82cf10b8a82373c93bddcf8317
yuma masuda <y.masuda1402+git97hub@gmail.com>
2018-09-30 16:02:23 +0900

    env: chenge js to ts

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as _ from 'lodash';
+
+declare const inkdrop: any;
+declare const app: any;
+
+const filePath = path.join(app.getPath('userData'), 'window-info.json');
+
+const restoreBounds = () => {
+  let windowInfo;
+
+  try {
+    windowInfo = JSON.parse(fs.readFileSync(filePath, {encoding: 'utf8'}));
+    if (_.isEqual(windowInfo, inkdrop.window.getBounds())) {
+      return;
+    }
+  } catch (e) {
+    windowInfo = {x: 0, y: 0, width: 800, height: 1000};
+  }
+
+  inkdrop.window.setBounds(windowInfo);
+};
+
+const init = () => {
+  restoreBounds();
+  // make window movable.
+  inkdrop.window.removeListener('maximize', init);
+};
+
+inkdrop.window.on('maximize', init);
+
+inkdrop.window.on('close', () => {
+  fs.writeFileSync(filePath, JSON.stringify(inkdrop.window.getBounds()));
+});
